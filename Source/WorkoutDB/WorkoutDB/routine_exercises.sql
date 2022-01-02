9aa934cf7ae26155e1646dc2f668d3cb7ef6a8da
mkaey <mkaettner@gmail.com>
2022-01-02 20:27:35 +0100

    Added Built Solution

--- Source/WorkoutDB/WorkoutDB/routine_exercises.sql
+++ Source/WorkoutDB/WorkoutDB/routine_exercises.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[routine_exercises]
 (
-	[routine_id] INT NOT NULL PRIMARY KEY, 
-    [exercise_id] INT NOT NULL PRIMARY KEY, 
+    [routine_exercises_id] INT NOT NULL,
+	[routine_id] INT NOT NULL, 
+    [exercise_id] INT NOT NULL, 
     CONSTRAINT [routine_exercises_routine_id] FOREIGN KEY ([routine_id]) REFERENCES [Routine]([routine_id]), 
     CONSTRAINT [routine_exercises_exercise_id] FOREIGN KEY ([exercise_id]) REFERENCES [Exercise]([exercise_id])
 )
